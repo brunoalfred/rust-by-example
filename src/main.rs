a0f6e1b90beaafd720d2436262056d9665d8f6ef
geek <alfredybruno@gmail.com>
2020-05-22 23:15:16 +0300

    feat: comparing the printable data for display and debug implementations.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,26 @@ impl fmt::Display for Structure {
     }
 }
 
+
+//for fmt::Debug
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+//for fmt::Display
+impl fmt::Display for MinMax{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+
+
 fn main(){
+
     println!("The age is {}", Structure(2));
+
+    let minmax = MinMax(0, 14);
+    println!("Compare the structure:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax );
 }
