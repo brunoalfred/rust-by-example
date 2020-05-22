f95ae9787cd1691a7f758a5cca4eb3e8596c39f4
geek <alfredybruno@gmail.com>
2020-05-22 21:50:09 +0300

    feat: special formating with `:`

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,11 @@ fn main() {
              subject = "the quick brown fox",
              verb = "jumps over"
     );
+
+    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
+
+
+    println!("{number:>width$}", number=1, width=6);
+
+    println!("{number:>0width$}", number=1, width=6);
 }
