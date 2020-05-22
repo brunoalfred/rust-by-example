ddc334d64f8aa825fb241b38256740349e15c90d
geek <alfredybruno@gmail.com>
2020-05-22 22:16:46 +0300

    feat: Printing using fmt:Debug.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
-fn main() {
-    struct UnPrintable(i32);
+// struct UnPrintable(i32);
 
-    #[derive(Debug)]
-    struct DebugPrintable(i32);
+//using `derive` attribute to make structure printable.
+#[derive(Debug)]
+struct DebugPrintable(i32);
+
+/*  Putting a `DebugPrintable` inside of structure `Deep` to make it
+  *Printable
+  */
+#[derive(Debug)]
+struct Deep(DebugPrintable);
+
+fn main() {
+
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+    "Slater",
+        "Christian",
+        actor="actor's"
+    );
+
 }
