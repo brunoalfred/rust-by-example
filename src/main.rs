2db70b55605529427856b4dc5744291bcc7a323b
geek <alfredybruno@gmail.com>
2020-05-22 23:26:04 +0300

    feat: comparing the printable data for display and debug implementations.

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,19 @@ impl fmt::Display for MinMax{
     }
 }
 
+//for fmt::Debug
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
 
+//for fmt::Display
+impl fmt::Display for Point2D{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
 
 fn main(){
 
@@ -31,4 +43,20 @@ fn main(){
     println!("Compare the structure:");
     println!("Display: {}", minmax);
     println!("Debug: {:?}", minmax );
+
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3,3);
+
+    println!("The big range is {big}, the small range is {small}",
+    big = big_range,
+    small = small_range
+    );
+
+
+    let point = Point2D{x: 2.5, y: 4.3};
+    println!("Compare the points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
 }
