5307815ac59500db104fe6e640ca347f00de26c3
geek <alfredybruno@gmail.com>
2020-05-22 22:50:55 +0300

    feat: Using the fmt::Display to print the value in structure type,

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,14 @@
-// struct UnPrintable(i32);
-
-//using `derive` attribute to make structure printable.
-#[derive(Debug)]
-struct DebugPrintable(i32);
-
-/*  Putting a `DebugPrintable` inside of structure `Deep` to make it
-  *Printable
-  */
-#[derive(Debug)]
-struct Deep(DebugPrintable);
-
-fn main() {
-
-    println!("{:?} months in a year.", 12);
-    println!("{1:?} {0:?} is the {actor:?} name.",
-    "Slater",
-        "Christian",
-        actor="actor's"
-    );
-
-    //The structure is now printable.
-    println!("Now will {:?} print !", DebugPrintable(3));
-
-    //To display only the result.
-    println!("Now the result is {:?}", Deep(DebugPrintable(5)));
+use std::fmt;
 
 
+struct Structure(i32);
+
+impl fmt::Display for Structure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}",self.0 )
+    }
+}
+
+fn main(){
+    println!("The age is {}", Structure(2));
 }
