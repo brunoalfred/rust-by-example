4d85f51f262cd9b762617f3840b306f1b4f7585b
geek <alfredybruno@gmail.com>
2020-05-22 23:59:22 +0300

    feat: Implemented the Euclid's Algorithm.

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,28 @@ impl fmt::Display for List {
 }
 
 
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+
+        }
+        m = m % n;
+
+    }
+    n
+}
+
+
 fn main() {
     let v = List(vec![1, 2, 3]);
 
     println!("{}", v);
+
+    let jibu = gcd(5, 10);
+    println!("{}", jibu );
 }
