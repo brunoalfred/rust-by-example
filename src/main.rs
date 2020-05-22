a3dbc3c518e4364d1a9745c9f4ce528b1faf2b93
geek <alfredybruno@gmail.com>
2020-05-22 21:42:01 +0300

    feat: Using named arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-
-fn main(){
-    let x = 5 + /* 90 + */ 5 ;
+fn main() {
+    let x = 5 + /* 90 + */ 5;
     println!("Is `x` 10 or 100? x = {}", x);
 
     println!("{0}, this is {1}. {1}, this is {0}", "Bruno", "Witty");
+
+    println!("{subject} {verb} {object}",
+             object = "the lazy dog",
+             subject = "the quick brown fox",
+             verb = "jumps over"
+    );
 }
