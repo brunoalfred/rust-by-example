e7888c833202fcb58a42ecfd29d8fe792f251dcc
geek <alfredybruno@gmail.com>
2020-05-22 22:08:00 +0300

    fix: shows the custom structure has to be implemented futher so as it can be printable with `fmt::Debug` or `fmt::Display`.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,6 @@
 fn main() {
-    let x = 5 + /* 90 + */ 5;
-    println!("Is `x` 10 or 100? x = {}", x);
+    struct UnPrintable(i32);
 
-    println!("{0}, this is {1}. {1}, this is {0}", "Bruno", "Witty");
-
-    println!("{subject} {verb} {object}",
-             object = "the lazy dog",
-             subject = "the quick brown fox",
-             verb = "jumps over"
-    );
-
-    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
-
-
-    println!("{number:>width$}", number=1, width=6);
-
-    println!("{number:>0width$}", number=1, width=6);
-
-    // println!("My name is {0}, {1}, {0}", "Bond");
-
-    struct Structure(i32);
-
-    println!("This struct `{}` won't print ...", Structure(3));
-
+    #[derive(Debug)]
+    struct DebugPrintable(i32);
 }
