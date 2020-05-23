e05c3d2c9e639b6c5a6c914400d62e4b02519474
geek <alfredybruno@gmail.com>
2020-05-23 21:40:17 +0300

    feat: Prompting the user to enter a value and handling it.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,15 @@
-use std::fmt;
-
-struct List(Vec<i32>);
-
-impl fmt::Display for List {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let vec = &self.0;
-
-        write!(f, "[")?;
-
-        for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
-            write!(f, "{}", v)?;
-        }
-        write!(f, "]")
-    }
-}
-
-
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-
-        }
-        m = m % n;
-
-    }
-    n
-}
-
+use std::io;
 
 fn main() {
-    let v = List(vec![1, 2, 3]);
 
-    println!("{}", v);
+    println!("Guess the number!");
+    println!("Please enter the Guess...");
 
-    let jibu = gcd(5, 10);
-    println!("{}", jibu );
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read the line!");
+
+    println!("You guessed: {}", guess);
+
+
 }
