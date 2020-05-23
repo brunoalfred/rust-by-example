291cd123e1834079cdaa4374dafdb9d0330461b9
geek <alfredybruno@gmail.com>
2020-05-23 23:18:14 +0300

    fix: dealing with input as a string and the random generated number so as they can be compared.

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,27 @@ use std::cmp::Ordering;
 
 
 fn main() {
-
     println!("Guess the number!");
     println!("Please enter the Guess...");
 
-
-
-
-
     let mut guess = String::new();
+
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read the line!");
+
+    let guess = guess.trim().parse()
+        .expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
     match guess.cmp(&secret_number) {
         Ordering::Greater => println!("Too small!"),
-        Ordering::Equal => println!("You win!"  ),
+        Ordering::Equal => println!("You win!"),
         Ordering::Less => println!("Too small"),
     }
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read the line!");
-
-    println!("You guessed: {}", guess);
-
-
 }
