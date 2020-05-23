8a3383248846796171c13de980f051e1bbb4c908
geek <alfredybruno@gmail.com>
2020-05-23 23:06:07 +0300

    feat: comparing the user's input and random generated value.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate rand;
 
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 
 fn main() {
@@ -10,10 +11,21 @@ fn main() {
     println!("Guess the number!");
     println!("Please enter the Guess...");
 
+
+
+
+
+    let mut guess = String::new();
+
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
-    let mut guess = String::new();
+    match guess.cmp(&secret_number) {
+        Ordering::Greater => println!("Too small!"),
+        Ordering::Equal => println!("You win!"  ),
+        Ordering::Less => println!("Too small"),
+    }
+
     io::stdin().read_line(&mut guess)
         .expect("Failed to read the line!");
 
