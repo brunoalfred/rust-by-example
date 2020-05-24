fd9ad5642696670cdd032a07634f1fd9e315cb8b
geek <alfredybruno@gmail.com>
2020-05-24 12:36:06 +0300

    feat: Printing values in a collection is less error prone with for construct since changing the length of collection doesnt destruct other parts of the code.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,10 @@
 fn main (){
 
     let array = [2, 3, 4, 5, 7, 7];
-    let mut index = 0;
 
-    while index < 6 {
-        println!("The values are: {}", array[index] );
-        index = index + 1;
 
-    }
+   for value in array.iter() {
+       println!("The value is: {}", value);
+   }
 
 }
