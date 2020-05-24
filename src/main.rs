23b524534edc690ccdea25f5de4328ab528378b4
geek <alfredybruno@gmail.com>
2020-05-24 12:21:33 +0300

    feat: Binding the value regarding the result of the if construct.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,12 @@
-extern crate rand;
+fn five() -> i32 {
+    5
+}
 
+fn main (){
 
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
+    let condition = true;
+    let number = if condition {
+        5
+    } else { 6 };
 
-
-fn main() {
-    println!("Guess the number!");
-    println!("Please enter the Guess...");
-
-    let mut guess = String::new();
-
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read the line!");
-
-    let guess = guess.trim().parse()
-        .expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
-
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Greater => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Less => println!("Too small"),
-    }
 }
