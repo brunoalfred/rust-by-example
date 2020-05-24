c4869cb58543d0ba238419818de01648a79da749
geek <alfredybruno@gmail.com>
2020-05-24 12:39:31 +0300

    feat: Using for to print values in a range.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-
-
-fn main (){
-
+fn main() {
     let array = [2, 3, 4, 5, 7, 7];
+    let number = 1..6;
 
 
-   for value in array.iter() {
-       println!("The value is: {}", value);
-   }
+    for value in array.iter() {
+        println!("The value is: {}", value);
+    }
 
+    for value in number {
+        println!("The values are: {}", value);
+    }
 }
