c8dedb6d4026acc28b7537e1dd656e3798e6cf1a
geek <alfredybruno@gmail.com>
2020-06-19 18:20:28 +0300

    error handling with option  and wrap

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-fn give_princess(gift: &str) {
 
-    if gift == "snake" {panic!("Aaaaaaaaaaaaaaah!")}
-    println!("I love {}sss!", gift);
 
+fn give_commoner(gift: Option<&str>){
+    //Specifying a course of action for each case of input.
+    match gift {
+        Some("snake") => println!("You are so courageous!"),
+        Some(inner) => println!("{}? How nice", inner),
+        None => println!("No gift?, what a shock"),
+
+        _ => {}
+    }
+}
+
+
+fn give_princess(gift: Option<&str>){
+    let inside = gift.unwrap();
+    if inside == "snake" {panic!("Aaaaaaaaaaaaah Mambo gani haya");}
+    else { println!("I love {}", inside); }
 
 }
-fn main() {
-    give_princess("teddy bear");
-    give_princess("snake");
+
+fn main(){
+
+    let food = Some("cabbage");
+    let snake = Some("Ugali");
+    let nothing = None;
+    give_commoner(food);
+    give_commoner(snake );
+
 }
