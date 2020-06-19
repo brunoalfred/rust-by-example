1c3f08e93ca2c8a07ac239f2414a4b3fc8586b10
geek <alfredybruno@gmail.com>
2020-06-19 18:33:05 +0300

    feat: Using Panic to handle error.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,19 @@
 
-
-fn give_commoner(gift: Option<&str>){
-    //Specifying a course of action for each case of input.
-    match gift {
-        Some("snake") => println!("You are so courageous!"),
-        Some(inner) => println!("{}? How nice", inner),
-        None => println!("No gift?, what a shock"),
-
+fn get_name(name: Option<&str>){
+    match name {
+        Some("Bruno") => println!(" The name is "),
+        None => panic!("Please give a name"),
         _ => {}
     }
 }
 
 
-fn give_princess(gift: Option<&str>){
-    let inside = gift.unwrap();
-    if inside == "snake" {panic!("Aaaaaaaaaaaaah Mambo gani haya");}
-    else { println!("I love {}", inside); }
 
-}
+
 
 fn main(){
 
-    let food = Some("cabbage");
-    let snake = Some("Ugali");
-    let nothing = None;
-    give_commoner(food);
-    give_commoner(snake );
+    let student_one = Some("Bruno");
+    get_name(student_one);
 
 }
