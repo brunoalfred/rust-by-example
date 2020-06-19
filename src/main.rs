c3e401fb9c7b6ae71626d00b8fbccf6fd0e97dbf
geek <alfredybruno@gmail.com>
2020-06-19 19:00:55 +0300

    feat: Nesting and labeling the loops.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,16 @@
 fn main() {
-    let n = 5;
+    'outer: loop {
+        println!("Entered the outer loop!");
 
-    if n < 0 {
-        print!("{} is negative", n);
-    } else if n > 0 {
-        print!("{} is positive", n);
-    } else {
-        print!("{} is zero", n);
+        'inner: loop {
+            println!("Entered the inner loop!");
+
+            break 'outer;
+        }
+
+        println!("Is this point ever reached! ");
     }
 
-    let big_n =
-        if n < 10 && n > -10 {
-            println!(", and is a small number, increase ten-fold");
 
-            // This expression returns an `i32`.
-            10 * n
-        } else {
-            println!(", and is a big number, halve the number");
-
-            // This expression must return an `i32` as well.
-            n / 2
-            // TODO ^ Try suppressing this expression with a semicolon.
-        };
-    //   ^ Don't forget to put a semicolon here! All `let` bindings need it.
-
-    println!("{} -> {}", n, big_n);
+    println!("Exited the outer loop!");
 }
