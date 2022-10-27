45f5dc0c1758e839f9bcbd6a706ec6eddc6d668d
Bruno Alfred <alfredybruno@gmail.com>
2022-10-27 10:35:58 +0300

    primitives

---
+++ src/primitives.rs
@@ -0,0 +1,29 @@
+// Tuples can be used as function arguments and as return values
+#[allow(dead_code)]
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    // `let` can be used to bind the members of a tuple to variables
+    let (int_param, bool_param) = pair;
+
+    (bool_param, int_param)
+}
+
+// The following struct is for the activity.
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+fn main() {
+    
+    // let reversed_pair = reverse((1, false));
+    // println!("reversed pair is {:?}", reversed_pair);
+    
+    // A tuple with a bunch of different types
+    let long_tuple = (
+        1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    );
+
+    // Values can be extracted from the tuple using tuple indexing
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+
+    
+}
